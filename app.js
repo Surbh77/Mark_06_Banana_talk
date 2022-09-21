89b66bd8ec46e3b8f0453d88f62a7daab1f1d9eb
Saurabh Shinde <108724393+Surbh77@users.noreply.github.com>
2022-09-21 15:09:17 +0530

    Ex4 Complete

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // console.log("Script is working from a different file!");
-// imput
-var username=prompt('Give me username');
+// // imput
+// var username=prompt('Give me username');
 
-// Process
-var welcomemessage='This Script is working!!'+username;
+// // Process
+// var welcomemessage='This Script is working!!'+username;
 
-alert(welcomemessage)
+// // output
+// alert(welcomemessage)
+var btntranslate=document.querySelector('#btn-translate')
+var txtinput=document.querySelector('#txt-input');
+console.log(txtinput)
+// console.log(btntranslate)
+
+function clickhandler(){
+    console.log('Clicked!')
+    console.log("Input:- "+txtinput.value)
+}
+
+btntranslate.addEventListener('click',clickhandler)
+

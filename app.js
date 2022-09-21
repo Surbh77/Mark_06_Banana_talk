902ed6ac0d0aac7197aee3248a4f2da91aa1f77c
Saurabh Shinde <108724393+Surbh77@users.noreply.github.com>
2022-09-21 12:21:29 +0530

    Import Javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+// console.log("Script is working from a different file!");
+// imput
+var username=prompt('Give me username');
+
+// Process
+var welcomemessage='This Script is working!!'+username;
+
+alert(welcomemessage)

62d3791294a7ace47d6d32b3be0fdcb2ef26c669
Saurabh Shinde <108724393+Surbh77@users.noreply.github.com>
2022-09-22 15:50:11 +0530

    Done without style

--- app.js
+++ app.js
@@ -10,15 +10,36 @@
 var btntranslate=document.querySelector('#btn-translate')
 var txtinput=document.querySelector('#txt-input');
 var outputdiv=document.querySelector('#output');
+// var serverurl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverurl="https://api.funtranslations.com/translate/minion.json"
 // outputdiv.innerText="Saurabh Shinde"
 
+function gettranslationurl(text){
+    return serverurl+"?"+"text="+text
+}
 console.log(outputdiv)
 
+// Defining error 
+function errorhandler(error){
+    console.log("Error occured",error)
+    alert("Something wrong with server! try after sometime")
+}
 
+// defining click
 function clickhandler(){
-    console.log('Clicked!')
-    console.log("Input:- "+txtinput.value)
-    outputdiv.innerText='ajsjsjsjsjsj   '+txtinput.value
+    // console.log('Clicked!')
+    // console.log("Input:- "+txtinput.value)
+  
+    var inputtext=txtinput.value; // taking input
+
+    // calling server for processing
+    fetch(gettranslationurl(inputtext))
+        .then(response=>response.json())
+        .then(json=> {
+            var translatedtext=json.contents.translated
+            outputdiv.innerText=translatedtext;
+        })
+    .catch(errorhandler)
 }
 
 btntranslate.addEventListener('click',clickhandler)
